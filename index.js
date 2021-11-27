cbdbc1c105a80e79552447d0b3c15143c99341f3
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-27 13:19:22 -0500

    trying map filter

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 // ## Lineup Rules
-
+// 1) The total salary of all players in a lineup may not exceed $45,000
 // 2) Lineups may not contain more than 2 players from a single team
 // 3) Lineups may not contain more than 3 players from a single game
 // 4) Lineups must contain exactly 3 players with the position of 'OF'
 // and must also contain exactly 1 player from each of the following positions: 
 // 'P', 'C', '1B', '2B', '3B', 'SS'
 
-
 const validateLineup = (lineup) => {
-  const payment = lineup.reduce((total, pay) => {
-    return total + pay.salary < 45000
-  }, 0)
+  const allGood = lineup.map((clear) => {
+    return ((clear.id + clear.name + clear.position + clear.teamId + (clear.salary)
+  })
 
-  return payment
+  return allGood
 }
-
-
 
 module.exports = validateLineup
