fb18c2d2b819247bc8184d00b30f4e8a017aba4c
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-28 13:34:08 -0500

    salary test met

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const validateLineup = (lineup) => {
-  // total 35600 (return flase)
-  const totalSal = lineup.reduce((currentSal, sal) => {
-    return sal.salary + currentSal
-  }, 0)
-
-  // return an array of all teamIds, and filter the teams with 2 or less players for single team
-  //which is an empty array 
-  const mapTeams = lineup.map(team => team.teamId).filter(team => team.length <= 2)
+  return !payment
 }
+
+const totalSal = (lineup) => {
+  return lineup.reduce((salary, player) => {
+    return salary + player.salary
+  }, 0)
+}
+
+const payment = (lineup) => {
+  return totalSal(lineup) > 45000
+}
+
+
+// filter teamIds, map teamId values, and reduce the values to one that are 2 or less
 
 
 module.exports = validateLineup
