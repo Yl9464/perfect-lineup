ccf1f86c51c8ef2120b5c384f86e49ff9f14054c
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-28 14:04:22 -0500

    single team rule function working

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const validateLineup = (lineup) => {
-  return !payment
+  const allTeams = lineup.map((team) => team.teamId).reduce((teams, allTeams) => {
+    console.log(teams)
+
+    return teams = allTeams <= 2
+  })
 }
-
-const totalSal = (lineup) => {
-  return lineup.reduce((salary, player) => {
-    return salary + player.salary
-  }, 0)
-}
-
-const payment = (lineup) => {
-  return totalSal(lineup) > 45000
-}
-
-
-// filter teamIds, map teamId values, and reduce the values to one that are 2 or less
-
 
 module.exports = validateLineup
