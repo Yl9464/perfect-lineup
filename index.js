12245609f260771f0ea86aa49a7f71473199dff5
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-28 11:56:51 -0500

    console logging teamIds

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const validateLineup = (lineup) => {
-  //total 35600 (return flase)
+  // total 35600 (return flase)
   const totalSal = lineup.reduce((currentSal, sal) => {
     return sal.salary + currentSal
   }, 0)
 
-  console.log(totalSal)
+  // return an array of all teamIds and filter the teams with 2 or less players for single team
+  lineup.forEach((team) => {
+    console.log(team.teamId)
+  })
 }
-
 
 
 
